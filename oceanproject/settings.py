ebb5c94b36546eff0006a61b308925ff1d5080fe
lexlopez-dev <a.lopezcr9@gmail.com>
2020-04-07 20:31:29 -0700

    module 7

--- oceanproject/settings.py
+++ oceanproject/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'oceanproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'oceanapp_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
